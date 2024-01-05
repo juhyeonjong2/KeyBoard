9db276351560df65da5d5a03a5fdf61020375e51
juhyeonjong <vkdlfjfxm2@naver.com>
2024-01-05 12:41:48 +0900

    데이터베이스 순서 수정

--- Documents/3. 설계/3. 데이터베이스/sql/member.sql
+++ Documents/3. 설계/3. 데이터베이스/sql/member.sql
@@ -3,14 +3,22 @@ use allkeyboard;
 
 create table member
 (
-	mno int unsigned not null primary key auto_increment comment '회원번호', 
+	mno int unsigned not null primary key unique auto_increment comment '회원번호', 
     mid varchar(255) not null comment '아이디', 
 	mname text not null comment '이름', 
     mphone varchar(13) not null comment '연락처',
     memail varchar(100) comment '이메일', 
     maddr text comment '주소',
-	rdate timestamp default now() comment '가입일', 
+	rdate timestamp comment '가입일', 
 	mpw char(32) not null comment '비밀번호',
     mlevel int comment '권한',
 	delyn char(1) comment '탈퇴여부' 
+);
+
+create table cert
+(
+ hash char(64) not null comment  '해쉬값',
+ expiretime timestamp comment '만료기간',
+ mno int  unsigned not null unique comment '회원번호',
+ foreign key(mno) references member(mno)
 );
